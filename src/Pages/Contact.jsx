0a6ebaa85eeeef087bb6348e7dca185f20d5486b
abhaypanditzx <abhaypanditzx@gmail.com>
2024-01-17 08:09:19 +0530

    ok

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -10,9 +10,8 @@ const Contact = () => {
           <input type="text" placeholder='what’s your good name?'   className='min-w-[250px] max-xs:w-[320px] max-[376px]:w-[300px] max-[325px]:w-[250px] md:w-[550px] sm:w-[500px] py-3 px-2 lg:w-[600px] text-white max-w-[600px] bg-black/20' />
           <input type="text" placeholder='what’s your web address?' className='min-w-[250px] max-xs:w-[320px] max-[376px]:w-[300px] max-[325px]:w-[250px] md:w-[550px] sm:w-[500px] py-3 px-2 lg:w-[600px] text-white max-w-[600px] bg-black/20' />
           <textarea name="" placeholder='enter your text here...'   className='min-w-[250px] max-xs:w-[320px] max-[376px]:w-[300px] max-[325px]:w-[250px] md:w-[550px] sm:w-[500px] lg:w-[600px] placeholder:p-4 text-white max-w-[600px] bg-black/20'id="" rows="4">
-
           </textarea>
-<button className='bg-Lgreen px-6  max-xs:py-1.5 py-2 lg:w-[150px] rounded-md text-lg text-white hover:scale-95 hover:bg-Lgreen/90 rubik font-[500] tracking-wide'>submit</button>
+          <button className='bg-Lgreen px-6  max-xs:py-1.5 py-2 lg:w-[150px] rounded-md text-lg text-white hover:scale-95 hover:bg-Lgreen/90 rubik font-[500] tracking-wide'>submit</button>
         </form>
       </div>
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
  
   return (
-    <div className={`h-screen fixed  top-0 flex home-main items-center p-10 w-full`}>
+    <div className={`h-screen fixed  top-0 flex  items-center p-10 w-full`}>
       <div className="flex flex-col  z-50 gap-y-4">
         <h1  className="text-white md:text-[5rem] xs:text-[4rem] uppercase max-xs:text-[3rem] font-rubikGlitch ">fast <span className="bg-[#28CD0D] px-3">race</span></h1>
         <h4 className="text-white text-[40px] font-[600]  max-xs:text-[20px]  max-xs:w-[300px] w-[570px] capitalize font-rubik">
@@ -24,6 +24,7 @@ const Home = () => {
       <div  className="black-gradient flex jusitfy-center  fixed  w-fit  z-20   max-xs:absolute right-0  items-center ">
         <img src={pngShoe} className="h-[400px] max-md:opacity-20 max-xs:h-[300px] " alt="shoe" />
       </div>
+      <img src={greenBg} className=" h-[400px] sm:w-[400px] max-xs:w-[300px]  lg:w-[500px] max-xs:bg-[300px] fixed bottom-[-100px] left-0 z-20"/>
     </div>
   );
 };
